a316497274c33e3e1f14c80d7d8bfda3ca447afd
Jinho Kim <jinho971031@gmail.com>
2021-03-16 14:42:58 +0900

    add for

--- portfolio/views.py
+++ portfolio/views.py
@@ -9,8 +9,8 @@ from django.utils import timezone
 #     return HttpResponse("Hello, world. You're at the port index>")
 
 def index(request):
-    posts = Post.objects.all()
-    return render(request, 'portfolio/index.html', {'message':posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'portfolio/index.html', {'posts':posts})
 
 def post_list(request):
     msg = 'Post_list'
@@ -19,18 +19,18 @@ def post_list(request):
 def cr_post(author, title, text):
     Post.objects.create(author=author, title=title, text=text)
 
-def use_query(request):
-    posts = Post.objects.all()
-    users = User.objects.all()
-    post_ft = Post.objects.filter(published_date=None)
-    print("post_ft: ", post_ft)
-    print("posts: ", posts)
-    print("users: ", users)
-    or_up = Post.objects.order_by('create_date')
-    print("or_up: ", or_up)
-    or_dw = Post.objects.order_by('-create_date')
-    print("or_dw: ", or_dw)
-    me = User.objects.get(username='jinho')
-    title = 'Test - 2'
-    text = 'Test - 2'
-    cr_post(me, title, text)
+# def use_query(request):
+    # posts = Post.objects.all()
+    # users = User.objects.all()
+    # post_ft = Post.objects.filter(published_date=None)
+    # print("post_ft: ", post_ft)
+    # print("posts: ", posts)
+    # print("users: ", users)
+    # or_up = Post.objects.order_by('create_date')
+    # print("or_up: ", or_up)
+    # or_dw = Post.objects.order_by('-create_date')
+    # print("or_dw: ", or_dw)
+    # me = User.objects.get(username='jinho')
+    # title = 'Test - 2'
+    # text = 'Test - 2'
+    # cr_post(me, title, text)
