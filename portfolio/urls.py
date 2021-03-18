10d6d9000d2c3be2bb517547f318498d8008e9f0
Jinho Kim <jinho971031@gmail.com>
2021-03-18 10:41:53 +0900

    test 0.01

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     # path('', views.index, name='index'),
     # post문자를 포함/정수값을 받아 pk라는 이름으로 view에 전달/ '/'가 나옴
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -12,7 +12,7 @@ from .forms import PostForm
 #     return HttpResponse("Hello, world. You're at the port index>")
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'portfolio/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -25,30 +25,31 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'portfolio/post_edit.html', {'form':form})
 
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST ,instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'portfolio/post_edit.html', {'form':form})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'portfolio/post_draft_list.html', {'posts':posts})
 
-def cr_post(author, title, text):
-    Post.objects.create(author=author, title=title, text=text)
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
 
-# def use_query(request):
-    # posts = Post.objects.all()
-    # users = User.objects.all()
-    # post_ft = Post.objects.filter(published_date=None)
-    # print("post_ft: ", post_ft)
-    # print("posts: ", posts)
-    # print("users: ", users)
-    # or_up = Post.objects.order_by('create_date')
-    # print("or_up: ", or_up)
-    # or_dw = Post.objects.order_by('-create_date')
-    # print("or_dw: ", or_dw)
-    # me = User.objects.get(username='jinho')
-    # title = 'Test - 2'
-    # text = 'Test - 2'
-    # cr_post(me, title, text)
